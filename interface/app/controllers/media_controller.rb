8356e762041317b646dca392d1314da5dbc1dbc1
Johan Brissmyr <brissmyr@gmail.com>
2009-04-21 16:55:38 +0000

    Ok, this code is ugly but we could at least expect some basic indentation...

--- interface/app/controllers/media_controller.rb
+++ interface/app/controllers/media_controller.rb
@@ -1,53 +1,49 @@
 class MediaController < ApplicationController
   require "dbus"
 
-      def play
-bus = DBus::SystemBus.instance
-service = bus.service("com.DogVibes.www")
-obj = service.object("/com/dogvibes/www")
-obj.introspect
-obj.default_iface = "com.DogVibes.www"
-obj.Play("gyllen", "bobdylan", params[:key])
-
-      end
-
-      def stop
-bus = DBus::SystemBus.instance
-service = bus.service("com.DogVibes.www")
-obj = service.object("/com/dogvibes/www")
-obj.introspect
-obj.default_iface = "com.DogVibes.www"
-obj.Stop("gyllen", "bobdylan")
-      end
-
-      def pause
-      end
-
-      def search
-bus = DBus::SystemBus.instance
-
-service = bus.service("com.DogVibes.www")
-obj = service.object("/com/dogvibes/www")
-
-songs = []
-
-obj.introspect
-
-if obj.has_iface? "com.DogVibes.www"
+  def play
+    bus = DBus::SystemBus.instance
+    service = bus.service("com.DogVibes.www")
+    obj = service.object("/com/dogvibes/www")
+    obj.introspect
     obj.default_iface = "com.DogVibes.www"
-    obj.Search("gyllen", "bobdylan", params[:q])[0].each { |song|
-      unless(song.empty?)
-        splitted = song.split(",")
-        s = {:title => splitted[1], :artist => splitted[0], :key => splitted[2]}
-	songs << s
-      end
-    }
-end
+    obj.Play("gyllen", "bobdylan", params[:key])
+  end
 
+  def stop
+    bus = DBus::SystemBus.instance
+    service = bus.service("com.DogVibes.www")
+    obj = service.object("/com/dogvibes/www")
+    obj.introspect
+    obj.default_iface = "com.DogVibes.www"
+    obj.Stop("gyllen", "bobdylan")
+  end
 
-#      render :json => [{:title => "Wonderwall", :artist => "Oasis", :key => "fzajIesf"},
-#                       {:title => "One", :artist => "Metallica", :key => "duU0samkdEe"}].to_json  
-      render :json => songs.to_json
-      end
+  def pause
+  end
+
+  def search
+    bus = DBus::SystemBus.instance
+
+    service = bus.service("com.DogVibes.www")
+    obj = service.object("/com/dogvibes/www")
+
+    songs = []
+
+    obj.introspect
+
+    if obj.has_iface? "com.DogVibes.www"
+      obj.default_iface = "com.DogVibes.www"
+      obj.Search("gyllen", "bobdylan", params[:q])[0].each { |song|
+        unless(song.empty?)
+          splitted = song.split(",")
+          s = {:title => splitted[1], :artist => splitted[0], :key => splitted[2]}
+          songs << s
+        end
+      }
+    end
+
+    render :json => songs.to_json
+  end
 
 end
