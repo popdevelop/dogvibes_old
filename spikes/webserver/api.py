6f04ae9fc43649345b1b37a2103fab3a84e091d7
Johan Brissmyr <brissmyr@gmail.com>
2009-05-18 04:28:44 +0000

    Call an arbitrary method with arguments from an URL

--- spikes/webserver/api.py
+++ spikes/webserver/api.py
@@ -1,16 +1,23 @@
 import BaseHTTPServer
+from urlparse import urlparse
 
-data = """{ error: 0 }"""
+class API:
+    def getStatus(self, query):
+        return "In getStatus: " + query
 
 class APIHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/amp/0/getStatus":
+        api = API()
+        u = urlparse(self.path)
+        method = u.path.split('/')[-1]
+        if hasattr(api, method):
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
+            data = getattr(api, method).__call__(u.query)
             self.wfile.write(data)
         else:
             self.send_error(404, 'Unsupported call')
-            
+
 httpserver = BaseHTTPServer.HTTPServer(("", 2000), APIHandler)
 httpserver.serve_forever()
