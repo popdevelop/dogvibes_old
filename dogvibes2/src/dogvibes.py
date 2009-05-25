57d4649032f0ea2c0cddb1fb26e7e713aebf60fe
Johan Brissmyr <brissmyr@gmail.com>
2009-05-25 18:51:24 +0000

    Moved playlists to separate file. The ugliest code around...

--- dogvibes2/src/dogvibes.py
+++ dogvibes2/src/dogvibes.py
@@ -1,6 +1,5 @@
 import gobject
 import gst
-import hashlib
 import os
 
 from amp import Amp
@@ -9,6 +8,7 @@ from amp import Amp
 from spotifysource import SpotifySource
 from lastfmsource import LastFMSource
 from filesource import FileSource
+from albumart import AlbumArt
 
 # import speakers
 from devicespeaker import DeviceSpeaker
@@ -53,33 +53,12 @@ class Dogvibes():
             ret += source.search(query)
         return ret
 
-    #import cStringIO
-    #from PIL import Image
-
-    def API_getAlbumArt(self, uri):
-        track = self.create_track_from_uri(uri)
-
-        art_dir = 'albumart'
-        if not os.path.exists(art_dir):
-            os.mkdir(art_dir)
-
-        img_hash = hashlib.sha224(track.artist + track.album).hexdigest()
-        img_path = art_dir + '/' + img_hash
-        if os.path.exists(img_path):
-            f = open(img_path, 'rb')
-            img_data = f.read()
-        else:
-            img_data = AlbumArtDownloader(track.artist, track.album).fetch()
-            f = open(img_path, 'wb')
-            f.write(img_data)
-
-        f.close()
-        return img_data
-
-        #im = cStringIO.StringIO(img_data)
-        #size = (159, 159)
-        #img = Image.open(im)
-        #img.thumbnail(size, Image.ANTIALIAS)
+    def API_getAlbumArt(self, uri, size = 0):
+        try:
+            track = self.create_track_from_uri(uri)
+            return AlbumArt.get_image(track.artist, track.album, size)
+        except DogError:
+            return AlbumArt.get_standard_image(size)
 
     def API_createPlaylist(self, name):
         Playlist.create(name)
@@ -106,48 +85,3 @@ class Dogvibes():
             track['id'] = uri[0]
             tracks.append(track)
         return tracks
-
-
-import re
-import urllib
-
-class AlbumArtDownloader(object):
-  awsurl = "http://ecs.amazonaws.com/onca/xml"
-  def __init__(self, artist, album):
-    self.artist = artist
-    self.album = album
-
-  def fetch(self):
-    url = self._GetResultURL(self._SearchAmazon())
-    if not url:
-      return None
-    img_re = re.compile(r'''registerImage\("original_image", "([^"]+)"''')
-    prod_data = urllib.urlopen(url).read()
-    m = img_re.search(prod_data)
-    if not m:
-      return None
-    img_url = m.group(1)
-    return urllib.urlopen(img_url).read()
-
-  def _SearchAmazon(self):
-    data = {
-      "Service": "AWSECommerceService",
-      "Version": "2005-03-23",
-      "Operation": "ItemSearch",
-      "ContentType": "text/xml",
-      "SubscriptionId": "AKIAIQ74I7SUW5COGZCQ",
-      "SearchIndex": "Music",
-      "ResponseGroup": "Small",
-    }
-
-    data["Artist"] = self.artist
-    data["Keywords"] = self.album
-
-    fd = urllib.urlopen("%s?%s" % (self.awsurl, urllib.urlencode(data)))
-
-    return fd.read()
-
-  def _GetResultURL(self, xmldata):
-    url_re = re.compile(r"<DetailPageURL>([^<]+)</DetailPageURL>")
-    m = url_re.search(xmldata)
-    return m and m.group(1)
