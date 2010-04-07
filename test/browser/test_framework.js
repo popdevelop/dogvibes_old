8b0f30a5c4c79cc7db2c857468a046129e2494c8
Johan Brissmyr <brissmyr@gmail.com>
2010-04-07 21:27:50 +0000

    * Benchmarking turn-around time

--- test/browser/test_framework.js
+++ test/browser/test_framework.js
@@ -1,6 +1,8 @@
-if (!('WebSocket' in window)) {
-    alert('WebSocket not available in your browser. Use Google Chrome')
-}
+//if (!('WebSocket' in window)) {
+//    alert('WebSocket not available in your browser. Use Google Chrome')
+//}
+
+
 
 var Log = function(){
     this.error = function(msg) {
@@ -12,6 +14,9 @@ var Log = function(){
     this.command = function(name, color, msg) {
         document.getElementById('log').innerHTML += '<font color="' + color + '"><b>' + msg + '</b></font><br>';
     };
+    this.result = function(color, command, reply, startTime, endTime, firstTime) {
+        document.getElementById("log").innerHTML += '<font color="' + color + '"><b>'+command+'</b><br>'+(startTime-firstTime) + 'ms' + ' (' + (endTime-startTime) +'ms): '+reply+'<i></i></font><br>';
+    };
     return this;
 }
 var log = new Log();
@@ -22,6 +27,12 @@ colors_nbr = 0;
 colors = []
 colors[0] = '#ff0000'
 colors[1] = '#0000ff'
+colors[2] = '#00dd00'
+colors[3] = '#ffff00'
+
+function pushHandler(d) {
+    // dummy
+}
 
 function Tester(name, uri)
 {
@@ -32,14 +43,21 @@ function Tester(name, uri)
 
     this.ws = new WebSocket(uri);
     this.ws.parent = this;
+
+    this.commands = []
+    this.replies = []
+    this.firstTime = 0;
+
 //    function getAllPlaylists(json) {
 //        //document.getElementById('log').innerHTML += json['result'][0]['name'] + '<br>';
 //    }
     this.ws.onopen = function() {};
     this.ws.onclose = function() {};
     this.ws.onmessage = function (e) {
-        log.reply(this.parent.name, this.parent.color, e.data)
-        //eval(e.data);
+        var reply = eval('('+e.data+')');
+        var t = new Date().getTime()
+        this.parent.replies.push({c: e.data, t: t, i: reply.msg_id});
+        //log.reply(this.parent.name, this.parent.color, e.data)
     };
 
     this.sendWS = function(cmd) {
@@ -48,7 +66,11 @@ function Tester(name, uri)
         } else {
             cmd += "&msg_id=" + this.msg_id;
         }
+        var t = new Date().getTime();
+        if (this.firstTime == 0)
+            this.firstTime = t;
         log.command(this.name, this.color, cmd)
+        this.commands.push({c: cmd, t: t, i: this.msg_id});
         this.ws.send(cmd);
         this.msg_id++;
     }
@@ -75,5 +97,33 @@ function start() {
     run();
 }
 function initTest() {
-    setTimeout('start()', 100);
+    setTimeout('start()', 500);
+}
+
+function findReplyFromCommand(command) {
+    for (var j = 0; j < testers.length; ++j) {
+        var t = testers[j];
+        for (var i = 0; i < t.replies.length; ++i) {
+            if (command.i == t.replies[i].i) {
+                return t.replies[i]
+            }
+        }
+    }
+    return null;
+}
+function showResults() {
+    setTimeout('x_showResults()', 2000);
+}
+function x_showResults() {
+    document.getElementById("log").innerHTML += '<h3>Results</h3>';
+    for (var j = 0; j < testers.length; ++j) {
+        var t = testers[j];
+        for (var i = 0; i < t.commands.length; ++i) {
+            var command = t.commands[i];
+            reply = findReplyFromCommand(command);
+            if (reply == null)
+                alert("Did not recieve reply! (Write test for this)");
+            log.result(t.color, command.c, reply.c, command.t, reply.t, t.firstTime);
+        }
+    }
 }
