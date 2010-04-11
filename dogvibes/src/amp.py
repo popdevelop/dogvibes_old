6c3d79c281fa0483129551ff6b8e68011bb40e53
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2010-04-11 14:28:53 +0000

    Sekk in correct manner. Let gstreamer handle flushing.

--- dogvibes/src/amp.py
+++ dogvibes/src/amp.py
@@ -217,9 +217,7 @@ class Amp():
         if self.src == None:
             return 0
         # FIXME: this *1000-hack only works for Spotify?
-        self.pipeline.seek_simple (gst.FORMAT_TIME, gst.SEEK_FLAG_NONE, int(mseconds) * 1000);
-        self.src.get_pad("src").push_event(gst.event_new_flush_start())
-        self.src.get_pad("src").push_event(gst.event_new_flush_stop())
+        self.pipeline.seek_simple (gst.FORMAT_TIME, gst.SEEK_FLAG_FLUSH, int(mseconds) * 1000);
         self.needs_push_update = True
 
     def API_setPlayQueueMode(self, mode):
